b28f399e868a734a8d60ad86989ba3e2bac93c5a
KBerezin <61414881+KBerezin@users.noreply.github.com>
2020-04-23 11:46:00 +0600

    add daoproperty

--- src/main/java/DAO/UserDAOFactory.java
+++ src/main/java/DAO/UserDAOFactory.java
@@ -2,26 +2,41 @@ package DAO;
 
 
 import util.DBHelper;
+import util.PropertyReader;
 
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class UserDAOFactory {
 
+    private static UserDAOType userDAOType;
     private static UserDAOFactory userDAOFactory;
 
     private UserDAOFactory() {
     }
 
-    public static synchronized UserDAOFactory getInstance(){
+    public static synchronized UserDAOFactory getInstance() {
         if (userDAOFactory == null) {
+            Properties properties = PropertyReader.getProperty("dao.properties");
+            if ("hibernate".equalsIgnoreCase(properties.getProperty("daotype"))) {
+                userDAOType = UserDAOType.HIBERNATE;
+            } else {
+                userDAOType = UserDAOType.JDBC;
+            }
             userDAOFactory = new UserDAOFactory();
         }
         return userDAOFactory;
     }
 
     public UserDAO getUserDAO() throws SQLException {
-        return new UserJDBCDAO(DBHelper.getInstance().getConnection());
-//        return new UserHiberDAO(DBHelper.getInstance().getSessionFactory());
+        if (userDAOType == UserDAOType.HIBERNATE) {
+            return new UserHiberDAO(DBHelper.getInstance().getSessionFactory());
+        } else {
+            return new UserJDBCDAO(DBHelper.getInstance().getConnection());
+        }
     }
 
+    public enum UserDAOType {
+        HIBERNATE, JDBC
+    }
 }
